f6aea7dd7f22558ae123906827b450d963a51a43
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-26 15:32:11 +0200

    verifica-utente

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,8 +1,17 @@
 //Lista 3 E-Mail
 const mailList = ['gigiomarchetti@gmail.com','martinaverdi@libero.it','gvmazzon25@gmail.com']
 console.log(mailList)
-//E-Mail dell'utente
 
+//E-Mail dell'utente
+const nameInput = document.getElementById('email');
+const accessBtn = document.querySelector('.btn-access');
+
+//verifica email utente
+accessBtn.addEventListener('click', function() {
+    //input
+    const user = nameInput.value.toLowerCase();
+    console.log(user)
+})
 
 
 //Controllare se l'E-Mail è nella lista
