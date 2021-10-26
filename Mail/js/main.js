ebd5f077a79a6afb40908745940b832564ed5fd5
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-26 15:18:09 +0200

    start-project-mail

---
+++ Mail/js/main.js
@@ -0,0 +1,10 @@
+//Lista 3 E-Mail
+const mailList = ['gigiomarchetti@gmail.com','martinaverdi@libero.it','gvmazzon25@gmail.com']
+console.log(mailList)
+//E-Mail dell'utente
+
+
+
+//Controllare se l'E-Mail è nella lista
+
+//Output
