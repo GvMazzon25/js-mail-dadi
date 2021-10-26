e9a0b0ccfac250054861a9f59bd798989d39b9ac
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-26 16:46:44 +0200

    output-end

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,6 +1,6 @@
 //Lista 3 E-Mail
 const mailList = ['gigiomarchetti@gmail.com','martinaverdi@libero.it','gvmazzon25@gmail.com']
-console.log(mailList);
+//console.log(mailList);
 
 //E-Mail dell'utente
 const nameInput = document.getElementById('email');
@@ -13,25 +13,25 @@ accessBtn.addEventListener('click', function() {
     console.log(user);
     
     //controllo inserimento
-
-    if( user === 0){
+    if( user === ''){
         alert("Inserire email");
+        console.log(user)
     }else{
         //Controllo presenza email in lista 
         let emailFound = false; //flag
-    }
-
-    for (i = 0; i < mailList.length; i++){
-       
-        //ottenere il dato nella email List
-        if(mailList[i] === user){
-            emailFound = true;
-            breack; 
+        console.log(emailFound)
+        for (i = 0; i < mailList.length; i++){
+            console.log(mailList[i])
+            //ottenere il dato nella email List
+            if(mailList[i] === user){
+                emailFound = true;
+                //Output
+                alert('Mail found')
+            }else{
+                alert('Mail not found')
+            }
         }
     }
 })
 
-    console.log()
-
-
-//Output
+   
